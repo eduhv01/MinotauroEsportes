128593699c879a14fb8b59ba5e6fe6aacb0d492a
Eduardo honório de Vasconcelos <138134896+eduhv01@users.noreply.github.com>
2024-11-17 12:58:27 -0300

    Add files via upload
    Subindo tela principal

---
+++ telaPrincipal/principal.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minotauro Esportes</title>
+    <link rel="stylesheet" href="assets/css/principal.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <div id="cookie-banner" class="cookie-banner">
+        <p>Usamos cookies para melhorar sua experiência. Ao continuar navegando, você concorda com o uso de cookies.</p>
+        <button id="accept-cookies">Aceitar</button>
+    </div>
+
+    <nav class="navbar">
+    <div class="logo">
+        <a href="index.php"><img src="assets/img/etc/logo.png" alt="Logo da Minotauro Esportes" class="logo-img" ></a>
+    </div>
+    <ul class="nav-links">
+        <li><a href="#">Perfil</a></li>
+        <li><a href="#">Comprar</a></li>
+        <li><a href="#">Saiba Mais</a></li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+</nav>
+
+<section class="hero">
+    <div class="hero-overlay">
+        <img src="assets/img/etc/pexels-cesar-galeao-1673528-3253501.jpg" alt="Banner Image" class="hero-img">
+        <div class="hero-text">
+            <h1>Bem-vindo à Minotauro Esportes!</h1>
+            <p>Onde atletas de alta performance encontram o melhor para alcançar seus limites e ir além. Desperte o campeão dentro de você com nossa linha exclusiva de suplementos premium, projetados para nutrir, fortalecer e transformar seu treino em resultados extraordinários.</p>
+            <button class="hero-btn">Explore Mais</button>
+        </div>
+    </div>
+</section>
+
+    <div class="main-content">
+    <h2>Produtos em Destaque</h2>
+    <div class="products">
+        <div class="product-card">
+            <img src="assets/img/produtos/coqueteleirapreta.webp" alt="Produto 1">
+            <h3>Coqueteleira SW</h3>
+            <p>Prepare suas bebidas e shakes de maneira prática e eficiente com a Coqueteleira SW. Com materiais de alta qualidade, ela oferece um design ergonômico e funcional.</p>
+            <button class="buy-btn">Comprar Agora</button>
+        </div>
+        <div class="product-card">
+            <img src="assets/img/produtos/creatina.jpeg" alt="Produto 2">
+            <h3>Creatina Integralmedica 300g</h3>
+            <p>É um suplemento de creatina monohidratada, essencial para quem busca melhorar seu desempenho atlético e aumentar a performance durante treinos de alta intensidade. Com a capacidade de fornecer energia extra aos músculos.</p>
+            <button class="buy-btn">Comprar Agora</button>
+        </div>
+        <div class="product-card">
+            <img src="assets/img/produtos/trembolona.png" alt="Produto 3">
+            <h3>Landerlan Drostanolona Trembolona Acetato</h3>
+            <p>é um poderoso esteroide anabolizante, frequentemente utilizado por atletas e bodybuilders que buscam aumento de força, massa muscular e desempenho em treinos intensos.</p>
+            <button class="buy-btn">Comprar Agora</button>
+        </div>
+        <div class="product-card">
+            <img src="assets/img/produtos/Durateston.jpeg" alt="Produto 4">
+            <h3>Durateston Plus Landerlan Gold</h3>
+            <p>É um suplemento de testosterona que combina diferentes ésteres para garantir uma liberação gradual do hormônio, maximizando ganhos musculares e força. Desenvolvido pela Landerlan.</p>
+            <button class="buy-btn">Comprar Agora</button>
+        </div>
+    </div>
+</div>
+
+<section class="reviews">
+    <h2>Avaliações dos Clientes</h2>
+    <div class="review-cards">
+        <div class="review-card">
+            <img src="assets/img/card/foto1.jpg" alt="João" class="client-photo">
+            <div class="review-content">
+                <p>"Produto incrível, melhor compra que fiz!"</p>
+                <span>- João, Atleta</span>
+            </div>
+        </div>
+        <div class="review-card">
+            <img src="assets/img/card/maria.jpeg" alt="Maria" class="client-photo">
+            <div class="review-content">
+                <p>"Ótima qualidade e entrega rápida!"</p>
+                <span>- Maria, Personal Trainer</span>
+            </div>
+        </div>
+        <div class="review-card">
+            <img src="assets/img/card/maratonista.jpeg" alt="Carlos" class="client-photo">
+            <div class="review-content">
+                <p>"Recomendo a todos os atletas!"</p>
+                <span>- Carlos, Maratonista</span>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="contact">
+        <h2>Fale Conosco</h2>
+        <form class="contact-form">
+            <input type="text" placeholder="Seu Nome" required>
+            <input type="email" placeholder="Seu Email" required>
+            <textarea placeholder="Sua Mensagem" required></textarea>
+            <button type="submit">Enviar</button>
+        </form>
+    </section>
+
+    <footer class="footer">
+        <div class="footer-links">
+            <a href="#">Política de Privacidade</a>
+            <a href="#">Termos de Serviço</a>
+            <a href="#">Suporte</a>
+        </div>
+        <p>&copy; 2024 Minotauro Esportes. Todos os direitos reservados.</p>
+    </footer>
+    <script src="assets/js/principal.js"></script>
+</body>
+</html>
