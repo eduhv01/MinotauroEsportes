2754a1d55985910bf4f0d1798aba49241a24897f
ArthurCOD459 <tutulacerda123@gmail.com>
2024-11-22 17:19:02 -0300

    Add files via upload

---
+++ carrossel/carrossel.php
@@ -0,0 +1,9 @@
+<div id="carrossel" class="carousel">
+    <div class="carousel-images">
+        <img src="telaPrincipal/assets/img/foto1.jpg" alt="Imagem 1">
+        <img src="telaPrincipal/assets/img/foto2.jpg" alt="Imagem 2">
+        <img src="telaPrincipal/assets/img/foto3.jpg" alt="Imagem 3">
+    </div>
+    <button class="prev">&#10094;</button>
+    <button class="next">&#10095;</button>
+</div>
