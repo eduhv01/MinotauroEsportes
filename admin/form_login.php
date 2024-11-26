5afa03146ee238c258d3eb96f9a9f9db137c4b55
Eduardo honório de Vasconcelos <138134896+eduhv01@users.noreply.github.com>
2024-11-26 08:28:59 -0300

    Update form_login.php

--- admin/form_login.php
+++ admin/form_login.php
@@ -5,20 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minotauro Esportes</title>
+    <title>Minotauro Esportes - Login</title>
     <link rel="stylesheet" href="login-configs/css/login.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar">
         <div class="logo">
-            <a href="principal.php"><img src="assets/img/etc/logo2.png" alt="Logo da Minotauro Esportes"
-                    class="logo-img"></a>
+            <a href="principal.php"><img src="assets/img/etc/logo2.png" alt="Logo da Minotauro Esportes" class="logo-img"></a>
         </div>
     </nav>
 
@@ -26,8 +23,8 @@
         <div class="login">
             <h1>Login</h1>
             <form action="login.php" method="post">
-                <input type="text" name="usuario" id="usuario" placeholder="usuario">
-                <input type="password" name="senha" id="senha" placeholder="Senha">
+                <input type="text" name="usuario" id="usuario" placeholder="Usuário" required>
+                <input type="password" name="senha" id="senha" placeholder="Senha" required>
                 <button type="submit">Entrar</button>
             </form>
         </div>
@@ -42,3 +39,5 @@
         <p>&copy; 2024 Minotauro Esportes. Todos os direitos reservados.</p>
     </footer>
 </body>
+
+</html>
