49ed754d53a205274ec20991118dfa2ae5c88031
ArthurCOD459 <tutulacerda123@gmail.com>
2024-11-29 22:38:58 -0300

    Update db_connection.php

--- carrossel_pagina_de_comentarios/carrossel/db_connection.php
+++ carrossel_pagina_de_comentarios/carrossel/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "admin";
-$password = "admin";
+$username = "root";
+$password = "";
 $dbname = "MinotauroEsportes";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
